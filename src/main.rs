298b2d5720d8b7fa177101f7252b8df5cffc6579
Warhorst <46809804+Warhorst@users.noreply.github.com>
2020-10-26 19:02:20 +0100

    Added yellow square that moves into its set direction.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
 use bevy::prelude::*;
 
+use pacman::PacmanPlugin;
+
+pub mod pacman;
+
 fn main() {
     App::build()
         .add_default_plugins()
-        .add_plugin(HelloPlugin)
+        .add_plugin(PacmanPlugin)
         .run()
-}
-
-struct Person;
-
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Person 1".to_string())))
-        .spawn((Person, Name("Person 2".to_string())))
-        .spawn((Person, Name("Person 3".to_string())));
-}
-
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, mut query: Query<(&Person, &Name)>) {
-    timer.0.tick(time.delta_seconds);
-    if timer.0.finished {
-        for (_person, name) in &mut query.iter() {
-            println!("hello {}", name.0)
-        }
-    }
-}
-
-struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
 }
---
+++ src/pacman.rs
@@ -0,0 +1,71 @@
+use bevy::prelude::*;
+
+pub struct PacmanPlugin;
+
+impl Plugin for PacmanPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(spawn_pacman.system())
+            .add_system(set_direction.system())
+            .add_system(move_pacman.system());
+    }
+}
+
+struct Pacman(Direction);
+
+enum Direction {
+    Idle,
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn spawn_pacman(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn(Camera2dComponents::default())
+        .spawn(SpriteComponents {
+            material: materials.add(Color::hex("FFEE00").unwrap().into()),
+            transform: Transform::from_translation(Vec3::new(-100.0, -215.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+            ..Default::default()
+        })
+        .with(Pacman(Direction::Idle));
+}
+
+fn set_direction(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Pacman>) {
+    for mut pacman in &mut query.iter() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            pacman.0 = Direction::Left
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) {
+            pacman.0 = Direction::Right
+        }
+
+        if keyboard_input.pressed(KeyCode::Up) {
+            pacman.0 = Direction::Up
+        }
+
+        if keyboard_input.pressed(KeyCode::Down) {
+            pacman.0 = Direction::Down
+        }
+    }
+}
+
+fn move_pacman(time: Res<Time>, mut query: Query<(&Pacman, &mut Transform)>) {
+    for (pacman, mut transform) in &mut query.iter() {
+        let translation = &mut transform.translation_mut();
+
+        let (x, y) = match &pacman.0 {
+            Direction::Up => (0.0, 1.0),
+            Direction::Down => (0.0, -1.0),
+            Direction::Left => (-1.0, 0.0),
+            Direction::Right => (1.0, 0.0),
+            Direction::Idle => (0.0, 0.0)
+        };
+
+        *translation.x_mut() += time.delta_seconds * x * 500.0;
+        *translation.x_mut() = translation.x().min(700.0).max(-700.0);
+        *translation.y_mut() += time.delta_seconds * y * 500.0;
+        *translation.y_mut() = translation.y().min(700.0).max(-700.0);
+    }
+}
