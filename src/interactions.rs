0e1def7d5e2d5d853f94c4f8199bf4f53c42a292
Warhorst <46809804+Warhorst@users.noreply.github.com>
2020-11-24 10:06:14 +0100

    Pacman now disappears when hit by a ghost.

--- src/interactions.rs
+++ src/interactions.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 use crate::common::Position;
+use crate::ghosts::Ghost;
 use crate::pacman::Pacman;
 use crate::points::Point;
 use crate::score::Score;
@@ -10,7 +11,9 @@ pub struct InteractionsPlugin;
 /// Plugin that controls interactions between actors from different categories.
 impl Plugin for InteractionsPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(pacman_eat_points.system());
+        app
+            .add_system(pacman_eat_points.system())
+            .add_system(ghost_hits_pacman.system());
     }
 }
 
@@ -20,6 +23,16 @@ fn pacman_eat_points(mut commands: Commands, mut score: ResMut<Score>, pacman_co
             if pacman_pos == point_pos {
                 score.increment();
                 commands.despawn(entity);
+            }
+        }
+    }
+}
+
+fn ghost_hits_pacman(mut commands: Commands, pacman_query: Query<With<Pacman, (Entity, &Position)>>, ghost_query: Query<With<Ghost, &Position>>) {
+    for (pacman_entity, pacman_position) in pacman_query.iter() {
+        for ghost_position in ghost_query.iter() {
+            if pacman_position == ghost_position {
+                commands.despawn(pacman_entity);
             }
         }
     }
