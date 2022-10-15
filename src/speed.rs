eebee5606d49bd67730c5c38ccc585353535409c
Warhorst <46809804+Warhorst@users.noreply.github.com>
2022-10-15 16:58:31 +0200

    Refactored the speed update

--- src/speed.rs
+++ src/speed.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use crate::board_dimensions::BoardDimensions;
+use bevy::ecs::query::WorldQuery;
 
 use crate::edibles::energizer::EnergizerTimer;
 use crate::ghosts::Ghost;
@@ -19,9 +20,8 @@ impl Plugin for SpeedPlugin {
         app
             .add_system_set(
                 SystemSet::on_update(LifeCycle::Running)
-                    .with_system(update_pacman_speed)
-                    .with_system(update_blinky_speed)
                     .with_system(update_ghost_speed)
+                    .with_system(update_pacman_speed)
             )
         ;
     }
@@ -31,30 +31,27 @@ impl Plugin for SpeedPlugin {
 #[derive(Copy, Clone, Component, Deref, DerefMut)]
 pub struct Speed(pub f32);
 
+#[derive(WorldQuery)]
+#[world_query(mutable)]
+struct GhostSpeedUpdateComponents<'a> {
+    ghost: &'a Ghost,
+    transform: &'a Transform,
+    speed: &'a mut Speed,
+    state: &'a State
+}
+
 fn update_ghost_speed(
     board: Res<Board>,
     level: Res<Level>,
+    eaten_dots: Res<EatenDots>,
     specs_per_level: Res<SpecsPerLevel>,
     dimensions: Res<BoardDimensions>,
-    mut query: Query<(&Ghost, &Transform, &mut Speed, &State)>
+    mut query: Query<GhostSpeedUpdateComponents>
 ) {
-    let ghost_speed = dimensions.ghost_base_speed();
-
-    for (ghost, transform, mut speed, state) in query.iter_mut() {
-        if ghost == &Blinky {
-            continue
-        }
-
-        let spec = specs_per_level.get_for(&level);
-
-        if *state == State::Eaten {
-            *speed = Speed(ghost_speed * 2.0)
-        } else if board.position_is_tunnel(&dimensions.trans_to_pos(transform)) {
-            *speed = Speed(ghost_speed * spec.ghost_tunnel_speed_modifier);
-        } else if *state == State::Frightened {
-            *speed = Speed(ghost_speed * spec.ghost_frightened_speed_modifier)
-        } else {
-            *speed = Speed(ghost_speed * spec.ghost_normal_speed_modifier)
+    for mut comps in query.iter_mut() {
+        match *comps.ghost {
+            Blinky => update_blinky_speed(&board, &level, &specs_per_level, &dimensions, &eaten_dots, &mut comps),
+            _ => update_non_blinky_speed(&board, &level, &specs_per_level, &dimensions, &mut comps)
         }
     }
 }
@@ -63,36 +60,50 @@ fn update_ghost_speed(
 /// gets two speed bonuses, depending on the remaining dots on the board.
 /// The amount of dots to trigger elroy depends on the current level.
 fn update_blinky_speed(
-    board: Res<Board>,
-    level: Res<Level>,
-    eaten_dots: Res<EatenDots>,
-    specs_per_level: Res<SpecsPerLevel>,
-    dimensions: Res<BoardDimensions>,
-    mut query: Query<(&Ghost, &Transform, &mut Speed, &State)>
+    board: &Board,
+    level: &Level,
+    specs_per_level: &SpecsPerLevel,
+    dimensions: &BoardDimensions,
+    eaten_dots: &EatenDots,
+    comps: &mut GhostSpeedUpdateComponentsItem
 ) {
     let ghost_speed = dimensions.ghost_base_speed();
+    let spec = specs_per_level.get_for(&level);
+    let remaining_dots = eaten_dots.get_remaining();
 
-    for (ghost, transform, mut speed, state) in query.iter_mut() {
-        if ghost != &Blinky {
-            continue
-        }
+    if *comps.state == State::Eaten {
+        *comps.speed = Speed(ghost_speed * 2.0)
+    } else if board.position_is_tunnel(&dimensions.trans_to_pos(comps.transform)) {
+        *comps.speed = Speed(ghost_speed * spec.ghost_tunnel_speed_modifier);
+    } else if *comps.state == State::Frightened {
+        *comps.speed = Speed(ghost_speed * spec.ghost_frightened_speed_modifier)
+    } else if remaining_dots <= spec.elroy_2_dots_left {
+        *comps.speed = Speed(ghost_speed * spec.elroy_2_speed_modifier)
+    } else if remaining_dots <= spec.elroy_1_dots_left {
+        *comps.speed = Speed(ghost_speed * spec.elroy_1_speed_modifier)
+    } else {
+        *comps.speed = Speed(ghost_speed * spec.ghost_normal_speed_modifier)
+    }
+}
 
-        let spec = specs_per_level.get_for(&level);
-        let remaining_dots = eaten_dots.get_remaining();
+fn update_non_blinky_speed(
+    board: &Board,
+    level: &Level,
+    specs_per_level: &SpecsPerLevel,
+    dimensions: &BoardDimensions,
+    comps: &mut GhostSpeedUpdateComponentsItem
+) {
+    let ghost_speed = dimensions.ghost_base_speed();
+    let spec = specs_per_level.get_for(&level);
 
-        if *state == State::Eaten {
-            *speed = Speed(ghost_speed * 2.0)
-        } else if board.position_is_tunnel(&dimensions.trans_to_pos(transform)) {
-            *speed = Speed(ghost_speed * spec.ghost_tunnel_speed_modifier);
-        } else if *state == State::Frightened {
-            *speed = Speed(ghost_speed * spec.ghost_frightened_speed_modifier)
-        } else if remaining_dots <= spec.elroy_2_dots_left {
-            *speed = Speed(ghost_speed * spec.elroy_2_speed_modifier)
-        } else if remaining_dots <= spec.elroy_1_dots_left {
-            *speed = Speed(ghost_speed * spec.elroy_1_speed_modifier)
-        } else {
-            *speed = Speed(ghost_speed * spec.ghost_normal_speed_modifier)
-        }
+    if *comps.state == State::Eaten {
+        *comps.speed = Speed(ghost_speed * 2.0)
+    } else if board.position_is_tunnel(&dimensions.trans_to_pos(comps.transform)) {
+        *comps.speed = Speed(ghost_speed * spec.ghost_tunnel_speed_modifier);
+    } else if *comps.state == State::Frightened {
+        *comps.speed = Speed(ghost_speed * spec.ghost_frightened_speed_modifier)
+    } else {
+        *comps.speed = Speed(ghost_speed * spec.ghost_normal_speed_modifier)
     }
 }
 
